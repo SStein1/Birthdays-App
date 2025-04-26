238648d7bccec46388ba617cb60ffd6e90101287
Shira Stein <sstein@jkha.org>
2025-04-26 15:35:36 -0400

    Part 1

--- Birthdays App/ContentView.swift
+++ Birthdays App/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = [
+        Friend(name: "Taylor Swift", birthday: .now),
+        Friend(name: "Selena Gomez", birthday: Date(timeIntervalSince1970: 0))
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(friends, id: \.name){friend in
+                HStack {
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
         }
-        .padding()
+            }
+            navigationTitle("Birthdays")
+        }
     }
 }
 
