3bd0bd34838b7cc087f4d3c42bb9e1749a78c677
Shira Stein <sstein@jkha.org>
2025-04-26 14:47:25 -0400

    Initial Commit

---
+++ Birthdays App/Birthdays_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Birthdays_AppApp.swift
+//  Birthdays App
+//
+//  Created by Shira Stein on 4/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Birthdays_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Birthdays AppTests/Birthdays_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Birthdays_AppTests.swift
+//  Birthdays AppTests
+//
+//  Created by Shira Stein on 4/26/25.
+//
+
+import Testing
+@testable import Birthdays_App
+
+struct Birthdays_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
